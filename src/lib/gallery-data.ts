3728350b2c22917f7da90a10c4270a37b0cd678a
Sourya Poudel <souryapoudel28@gmail.com>
2025-11-19 15:15:42 +0000

    still no change

--- src/lib/gallery-data.ts
+++ src/lib/gallery-data.ts
@@ -2,7 +2,7 @@
 export const photos = [
   { src: 'https://i.ibb.co/7d75mDm/image.png', hint: 'couple smiling', caption: 'The day we first smiled together. Dec 20, 2023' },
   { src: 'https://i.ibb.co/TMCB1yR/IMG-20250623-211920.jpg', hint: 'holding hands', caption: 'Our First Time Holding Hands Sharing Couple Bracelets. Apr 7, 2025' },
-  { src: 'https://i.ibb.co/d4pTWKNY/IMG-20250623-210321.jpg', hint: 'beach sunset', caption: 'The most precious photo I have ever seen. Aug 25, 2025' },
+  { src: 'https://i.ibb.co/XZrC4cs/image-2025-11-19-204936220.png', hint: 'beach sunset', caption: 'The most precious photo I have ever seen. Aug 25, 2025' },
   { src: 'https://i.ibb.co/KTW8mcf/image-2025-06-23-211046853.png', hint: 'city walk', caption: 'Quick click before the OPs arrived, ;) Aug 25, 2025' },
   { src: 'https://placehold.co/400x600.png', hint: 'coffee date', caption: 'Countless coffee dates. Apr 22, 2024' },
   { src: 'https://placehold.co/600x400.png', hint: 'mountain hike', caption: 'Adventures in the mountains. May 30, 2024' },
