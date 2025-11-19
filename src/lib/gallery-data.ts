4aa5dc5260f4c139f595b4b7a7b57129c66aa5f7
Sourya Poudel <souryapoudel28@gmail.com>
2025-11-19 15:31:48 +0000

    image:1  Failed to load resource: the server responded with a status of

--- src/lib/gallery-data.ts
+++ src/lib/gallery-data.ts
@@ -1,9 +1,7 @@
 
 export const photos = [
   { src: 'https://i.ibb.co/KTW8mcf/image-2025-06-23-211046853.png', hint: 'couple smiling', caption: 'The day we first smiled together. Dec 20, 2023' },
-  { src: 'https://i.ibb.co/TMCB1y1R/IMG-20250623-211920.jpg', hint: 'holding hands', caption: 'Our First Time Holding Hands Sharing Couple Bracelets. Apr 7, 2025' },
-  { src: 'https://i.ibb.co/HLxbvNCH/image-2025-11-19-204956802.png', hint: 'frontdesk', caption: 'Quick click before the OPs arrived, ;) Aug 25, 2025' },
-  { src: 'https://placehold.co/400x600.png', hint: 'coffee date', caption: 'Countless coffee dates. Apr 22, 2024' },
+  { src: 'https://picsum.photos/seed/2/600/400', hint: 'holding hands', caption: 'Our First Time Holding Hands Sharing Couple Bracelets. Apr 7, 2025' },
   { src: 'https://placehold.co/600x400.png', hint: 'mountain hike', caption: 'Adventures in the mountains. May 30, 2024' },
   { src: 'https://placehold.co/600x400.png', hint: 'baking together', caption: 'Making a mess in the kitchen. Jun 18, 2024' },
   { src: 'https://placehold.co/400x600.png', hint: 'park picnic', caption: 'A perfect picnic day. Jul 05, 2024' },
