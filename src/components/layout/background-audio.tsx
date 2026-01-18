8fb998a6ba0125a8407198a3a095b012de0bc5b0
Sourya Poudel <souryapoudel28@gmail.com>
2026-01-18 13:49:44 +0000

    https://open.spotify.com/track/6qqrTXSdwiJaq8SO0X2lSe?si=b4c95614b87c4cd

--- src/components/layout/background-audio.tsx
+++ src/components/layout/background-audio.tsx
@@ -1,15 +1,14 @@
-
 'use client';
 
 export default function BackgroundAudio() {
   return (
-    <div className="fixed bottom-4 left-4 z-50 w-72 hidden md:block">
+    <div className="fixed bottom-4 left-4 z-50 w-80 hidden md:block">
          <iframe
             title="Spotify Player"
             style={{borderRadius: '12px'}}
-            src="https://open.spotify.com/embed/track/6qqrTXSdwiJaqrSO0X2lSe?utm_source=generator&theme=0"
+            src="https://open.spotify.com/embed/track/6qqrTXSdwiJaq8SO0X2lSe?utm_source=generator&theme=1"
             width="100%"
-            height="80"
+            height="152"
             frameBorder="0"
             allowFullScreen={false}
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
