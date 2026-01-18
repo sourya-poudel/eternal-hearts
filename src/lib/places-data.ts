2898e1e00069692ca5810470690b0099ed50dbbc
Sourya Poudel <souryapoudel28@gmail.com>
2026-01-18 13:28:24 +0000

    where we first met: https://maps.app.goo.gl/AWtpsGaZoTLv3NHp9
    first dat

--- src/lib/places-data.ts
+++ src/lib/places-data.ts
@@ -1,20 +1,20 @@
 export const places = [
   {
     title: 'Where We Met',
-    location: 'A quiet library corner',
-    description: 'In between stacks of books and hushed whispers, our story began. It was a classic movie scene, and we were the stars.',
-    embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.284297129481!2d-122.42193588468209!3d37.78342497975815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858099a4993175%3A0x28d3f769d033a388!2sSan%20Francisco%20Public%20Library!5e0!3m2!1sen!2sus!4v1678886400000!5m2!1sen!2sus'
+    location: 'Bhubaneswar, Odisha, India',
+    description: 'In the heart of the city, under the vast open sky, our journey began. A simple meeting that sparked a lifetime of love.',
+    embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d239486.8166567533!2d85.65934537155631!3d20.30113888325695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a1909d2d5170aa5%3A0xfc580e2b68b33fa8!2sBhubaneswar%2C%20Odisha%2C%20India!5e0!3m2!1sen!2sus!4v1680000000000!5m2!1sen!2sus'
   },
   {
     title: 'Our First Date',
-    location: 'A cozy coffee shop',
-    description: 'Nervous sips of coffee and hours of conversation that felt like minutes. This is where we knew this was something special.',
-    embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.218531191024!2d-122.4359392846819!3d37.78499897975736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858099e28f35e3%3A0xf6a8c20f7a7f7b31!2sThe%20Mill!5e0!3m2!1sen!2sus!4v1678886500000!5m2!1sen!2sus'
+    location: 'Big Bro\'s Cafe, Bhubaneswar',
+    description: 'Over coffee and conversation, we discovered a connection that felt like coming home. A perfect first chapter.',
+    embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3742.361957019681!2d85.81432421491956!3d20.28589088640578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a1909edb7c89737%3A0x6e9a9b7b9986b625!2sBig%20Bro%27s%20Cafe!5e0!3m2!1sen!2sus!4v1680000000001!5m2!1sen!2sus'
   },
   {
     title: 'The Proposal',
-    location: 'A scenic city overlook',
-    description: 'On a starry night, with the city lights sparkling below, we promised each other forever. A perfect moment at a perfect place.',
-    embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3154.275529125553!2d-122.4491953846828!3d37.76137697976216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858752490263f3%3A0x5a63901b4c34614b!2sTwin%20Peaks!5e0!3m2!1sen!2sus!4v1678886600000!5m2!1sen!2sus'
+    location: 'The Black Eagle, Bhubaneswar',
+    description: 'A promise of forever, made in a place as special as our love. This is where our next chapter began.',
+    embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3742.87118318237!2d85.8016489149192!3d20.26463998641776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a19a70655555555%3A0x892a0139589993e!2sThe%20Black%20Eagle!5e0!3m2!1sen!2sus!4v1680000000002!5m2!1sen!2sus'
   }
 ];
