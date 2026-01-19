935ba612aa9d4df7157945fccdc370639de700c8
Sourya Poudel <90968372+sourya-poudel@users.noreply.github.com>
2026-01-19 18:58:34 +0545

    Title Update

--- src/lib/gallery-data.ts
+++ src/lib/gallery-data.ts
@@ -1,15 +1,15 @@
 
 export const photos = [
-  { src: 'https://i.ibb.co/1JsNVcL2/Whats-App-Image-2026-01-18-at-18-41-27.jpg', hint: 'couple smiling', caption: 'The day we first smiled together. Dec 20, 2023' },
-  { src: 'https://i.ibb.co/BHyJ05K2/Whats-App-Image-2026-01-18-at-18-41-41.jpg', hint: 'couple selfie', caption: 'Our First Time Holding Hands Sharing Couple Bracelets. Apr 7, 2025' },
+  { src: 'https://i.ibb.co/1JsNVcL2/Whats-App-Image-2026-01-18-at-18-41-27.jpg', hint: 'couple smiling', caption: 'The day we first smiled together. Jan 14, 2026' },
+  { src: 'https://i.ibb.co/BHyJ05K2/Whats-App-Image-2026-01-18-at-18-41-41.jpg', hint: 'couple selfie', caption: 'Food Fest Fiesta. Dec 29, 2025' },
   { src: 'https://i.ibb.co/HLxbvNCH/image-2025-11-19-204956802.png', hint: 'frontdesk', caption: 'Quick click before the OPs arrived, ;) Aug 25, 2025' },
-  { src: 'https://i.ibb.co/7cQ97ML/image-2025-11-19-204946273.png', hint: 'coffee date', caption: 'Countless coffee dates. Apr 22, 2024' },
-  { src: 'https://i.ibb.co/4nnGB3jT/image-2025-11-19-204917576.png', hint: 'mountain hike', caption: 'Adventures in the mountains. May 30, 2024' },
-  { src: 'https://i.ibb.co/6Js1jTBx/image-2025-11-19-205007935.png', hint: 'baking together', caption: 'Making a mess in the kitchen. Jun 18, 2024' },
+  { src: 'https://i.ibb.co/7cQ97ML/image-2025-11-19-204946273.png', hint: 'coffee date', caption: 'Mirror Magic. Apr 24, 2025' },
+  { src: 'https://i.ibb.co/4nnGB3jT/image-2025-11-19-204917576.png', hint: 'mountain hike', caption: 'With the ones that mean the most. Aug 23, 2024' },
+  { src: 'https://i.ibb.co/6Js1jTBx/image-2025-11-19-205007935.png', hint: 'baking together', caption: 'Staircase Photoshoot?. Aug 25, 2024' },
   { src: 'https://i.ibb.co/PGhsSP0r/Screenshot-2026-01-18-184215.png', hint: 'couple photo', caption: 'Another great day together.' },
   { src: 'https://i.ibb.co/1JsNVcL2/Whats-App-Image-2026-01-18-at-18-41-27.jpg', hint: 'happy moments', caption: 'Cherished memories we made.' },
   { src: 'https://i.ibb.co/cc48LhYg/Whats-App-Image-2026-01-18-at-18-40-26.jpg', hint: 'couple together', caption: 'Just the two of us.' },
-  { src: 'https://i.ibb.co/PzfDKvGN/Whats-App-Image-2026-01-18-at-18-40-19-1.jpg', hint: 'love story', caption: 'Our love story in a single frame.' },
+  { src: 'https://i.ibb.co/PzfDKvGN/Whats-App-Image-2026-01-18-at-18-40-19-1.jpg', hint: 'love story', caption: 'Holding Hands For Eternity.' },
   { src: 'https://i.ibb.co/4ZPGYyHL/Whats-App-Image-2026-01-18-at-18-40-19.jpg', hint: 'smiling couple', caption: 'Always smiling when I am with you.' },
   { src: 'https://i.ibb.co/HvWrbHw/Whats-App-Image-2026-01-18-at-18-40-18.jpg', hint: 'special moment', caption: 'A day I will always remember.' },
 ];
